be3c2eeb197d0003e53f4a920a5543d908e07033
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-09-03 21:20:00 +0800

    Update look_status.sh

--- look_status.sh
+++ look_status.sh
@@ -5,7 +5,7 @@ status=$(pm2 show cysic-aleo-prover | grep "status" | awk '{print $4}')
 
 # 检查状态
 if [ "$status" == "online" ]; then
-    echo "cysic-aleo-prover is online! ☀️"
+    echo "cysic_aleo正在运行"
 else
-    echo "cysic-aleo-prover is offline! ❌"
+    echo "停止"
 fi
